af147552265ef4e568ac55f7202554a539f61210
Liliana Pereira <lpereirapro@gmail.com>
2019-02-03 12:18:08 +0000

    Auto stash before merge of "master" and "origin/master"

--- frontend/recoverPasswordService.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once $_SERVER['DOCUMENT_ROOT'].'/helloprint/vendor/autoload.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/helloprint/backend/src/RecoverPassword.php';
-
-use \Helloprint\Services\RecoverPassword;
-
-$recoverPass = new RecoverPassword();
-
-return recover($_GET['username']);
