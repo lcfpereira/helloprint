983b2128490638677460e97e383e1e601da4dc84
Liliana Pereira <lilianapereira.mindsource@edp.pt>
2019-02-24 12:57:39 +0000

    rabbit mq connection

--- broker/src/index.php
+++ broker/src/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('172.17.0.2', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 $channel->queue_declare('hello', false, false, false, false);
 
